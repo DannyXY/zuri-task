d2b10606f6ceb2d398bcfc15d1621e73a2ead151
Adewambe Daniel <danoscolobo@gmail.com>
2022-10-27 17:54:11 +0100

    fixed-bugs

--- server.js
+++ server.js
@@ -1,27 +1,21 @@
-var http = require("http");
+var express = require("express");
+var server = express();
+var cors = require("cors");
 require("dotenv").config();
 
-var server = http.createServer(function (req, res) {
-  const data = {
-    slackUsername: "dannyx",
-    backend: true,
-    age: 19,
-    bio: "Quick learner, interested in new things",
-  };
-  if (req.method == "GET") {
-    if (req.url == "/details") {
-      res.writeHead(200, { "Content-type": "application/json" });
-      res.write(JSON.stringify(data));
+const data = {
+  slackUsername: "dannyx",
+  backend: true,
+  age: 19,
+  bio: "Quick learner, interested in new things",
+};
 
-      res.end();
-    } else {
-      res.end("we're not fully scaled at the moment");
-    }
-  } else {
-    res.end("this method isn't allowed for this route");
-  }
+server.use(cors());
 
+server.get("/details", (req, res) => {
+  res.status(200).json(data);
 });
 
-server.listen(process.env.PORT);
-console.log(`Server is listening on port ${process.env.PORT}`);
+server.listen(process.env.PORT, () => {
+  console.log(`server is listening on port ${process.env.PORT}`);
+});
