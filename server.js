f3ccbf24b04a698282426153caaba7651d67cbb1
Adewambe Daniel <danoscolobo@gmail.com>
2022-10-27 09:05:19 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+var http = require("http");
+require("dotenv").config();
+
+var server = http.createServer(function (req, res) {
+  const data = {
+    slackUsername: "dannyx",
+    backend: true,
+    age: 19,
+    bio: "Quick learner, interested in new things",
+  };
+  if (req.url == "/details") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.write(JSON.stringify(data));
+
+    res.end();
+  } else {
+    res.end("we're not fully scaled at the moment");
+  }
+});
+
+server.listen(process.env.PORT);
+console.log(`Server is listening on port ${process.env.PORT}`);
